b511105abca9b6d2e7bfda220e65683a94481119
Ian Porada <ianporada@gmail.com>
2024-01-06 11:24:02 -0500

    update preprocessing

--- preprocessing/copying_datasets.py
+++ preprocessing/copying_datasets.py
@@ -5,16 +5,7 @@ filtered_dataset = dataset.filter(lambda example: 'wino' in example["task_name"]
 
 winogrande_dataset = filtered_dataset.filter(lambda example: 'winogrande' in example["task_name"])
 
-{
-    'task029_winogrande_full_object', # Creating a pair of fill in the blank question-answer pairs on objects.	
-    'task030_winogrande_full_person', # Creating a pair of fill in the blank questions on persons.	
-    'task031_winogrande_question_generation_object', # Writing a fill in the blank question on objects.	
-    'task032_winogrande_question_generation_person', # Writing a fill in the blank question on persons.	
-    'task033_winogrande_answer_generation', # Answering a fill in the blank question on objects.	# only debiased
-    'task034_winogrande_question_modification_object', # Modifying a fill in the blank question on objects.	
-    'task035_winogrande_question_modification_person', # Modifying a fill in the blank question on persons.	
-    'task1391_winogrande_easy_answer_generation', # Answering a fill in the blank question on objects.	
- }
+
 
 
 
